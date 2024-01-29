1651d52931dccec325292222ba943ba11fccbcdb
blazerpaul15 <blazerpaul15@gmail.com>
2024-01-29 17:51:34 +0900

    Revert "techpack: msm: dsi: Implement fps gamma update"
    This reverts commit 997fa7594700463525793ac5659c24aa361c53eb.

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -285,7 +285,6 @@ struct dsi_panel {
 	bool fod_hbm_requested;
 	bool fod_ui;
 	int local_hbm_on_1000nit_51_index;
-	int cached_fps;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -391,7 +390,6 @@ int dsi_panel_mode_switch_to_vid(struct dsi_panel *panel);
 int dsi_panel_switch(struct dsi_panel *panel);
 
 int dsi_panel_post_switch(struct dsi_panel *panel);
-int dsi_panel_gamma_switch(struct dsi_panel *panel);
 
 void dsi_dsc_pclk_param_calc(struct msm_display_dsc_info *dsc, int intf_width);
 
